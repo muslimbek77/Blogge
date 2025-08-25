35b01b0941cd9d8fffdb88d611892f4a4565328f
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-08-25 10:05:32 +0500

    add contact view

--- blogge/views.py
+++ blogge/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from .forms import ContactForm
 
 def home_page(request):
     return render(request=request, template_name="index.html")
 
 def contact_page(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
     return render(request=request,template_name='contact.html')
 
 def about_page(request):
