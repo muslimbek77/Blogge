21920243e43963f050053b29473cb36d41bc682e
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-08-25 09:01:44 +0500

    add about & contact page

--- blogge/views.py
+++ blogge/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request=request, template_name="index.html")
+
+def contact_page(request):
+    return render(request=request,template_name='contact.html')
+
+def about_page(request):
+    return render(request=request,template_name='about.html')
