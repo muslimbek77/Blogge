4af4888a1f636a28ec462f679692a68ba9995941
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-08-29 09:11:23 +0500

    add blog list view

--- blogge/views.py
+++ blogge/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import ContactForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Blog
 
-def home_page(request):
-    return render(request=request, template_name="index.html")
+# def home_page(request):
+#     articles = Blog.objects.all()
+#     context = {'articles':articles}
+#     return render(request=request, template_name="index.html",context=context)
+
+class HomePageView(ListView):
+    model = Blog
+    context_object_name = 'articles'
+    template_name = 'index.html'
+    paginate_by = 7
+
 
 def contact_page(request):
     if request.method == 'POST':
