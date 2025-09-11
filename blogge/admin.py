a528a02948fb1c3d0900007d0ed27cf2f4074ed7
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-09-11 16:48:25 +0500

    add optimization admin panel, add models.py optimizate

--- blogge/admin.py
+++ blogge/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Contact,Blog,Category
+from django.utils.html import format_html
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["img","title","category"]
+    readonly_fields = ["view_count","created_date"]
+    list_filter=["category"]
+    search_fields = ["title"]
+    
+    def img(self, obj):
+         return format_html('<img width="100" src="{}" />'.format(obj.main_photo.url))
 
-admin.site.register(Contact)
-admin.site.register(Blog)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+     list_display = ["name","email"]
 admin.site.register(Category)
--- blogge/models.py
+++ blogge/models.py
@@ -2,28 +2,41 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 class Contact(models.Model):
-    name = models.CharField(max_length= 255)
-    email = models.EmailField(max_length=255)
-    message = models.TextField()
+    name = models.CharField(max_length= 255,verbose_name="Ism")
+    email = models.EmailField(max_length=255,verbose_name="Email")
+    message = models.TextField(verbose_name="Xabar")
 
     def __str__(self):
         return f"{self.name} - {self.email}"
+    class Meta:
+        verbose_name = ("Kontakt")
+        verbose_name_plural = ("Kontaktlar")
+
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,verbose_name="Kategoriya nomi")
     
     def __str__(self) -> str:
         return self.name
+    class Meta:
+        verbose_name = ("Kategoriya")
+        verbose_name_plural = ("Kategoriyalar")
+
 
 class Blog(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255,verbose_name="Sarlavha")
     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
-    author = models.CharField(max_length=255)
-    created_date = models.DateTimeField(auto_now=True)
-    main_photo = models.ImageField(upload_to="BlogPhotos/")
-    content = RichTextField()
-    view_count = models.IntegerField(default=0) #ko'rishlar soni
+    author = models.CharField(max_length=255,verbose_name="Avtor")
+    created_date = models.DateTimeField(auto_now=True,verbose_name="Yaratilgan sana")
+    main_photo = models.ImageField(upload_to="BlogPhotos/",verbose_name="Rasm")
+    content = RichTextField(verbose_name="Maqola matni")
+    view_count = models.IntegerField(default=0,verbose_name="Ko'rishlar soni") #ko'rishlar soni
 
     def __str__(self) -> str:
         return self.title
+
+    class Meta:
+        verbose_name = ("Blog")
+        verbose_name_plural = ("Bloglar")
 
